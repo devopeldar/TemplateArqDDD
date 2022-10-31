bb1f325cbfdc0f607cbba96726615baf1dbd45e8
Luis David Gauchat <luisgauchat@hotmail.com>
2022-10-31 11:32:22 -0300

    dockerfile y program modificados

--- TemplateArqDDD.Servicio.WebApi/Program.cs
+++ TemplateArqDDD.Servicio.WebApi/Program.cs
@@ -4,13 +4,11 @@ using TemplateArqDDD.Servicio.WebApi.Modulos.Mapper;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddMapper();//no viene en la plantilla WEBAPI
+
 builder.Services.AddInjection(builder.Configuration);//no viene en la plantilla WEBAPI
 
 builder.WebHost.ConfigureKestrel(options => //no viene en la plantilla WEBAPI
@@ -20,13 +18,6 @@ builder.WebHost.ConfigureKestrel(options => //no viene en la plantilla WEBAPI
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
 
 app.UseSwaggerUI();
 
