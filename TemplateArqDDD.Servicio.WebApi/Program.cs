604922f0ecafecbb0911667bb954a3a19a81ab07
Luis David Gauchat <luisgauchat@hotmail.com>
2022-10-31 11:23:50 -0300

    se agrega dockerfile

--- TemplateArqDDD.Servicio.WebApi/Program.cs
+++ TemplateArqDDD.Servicio.WebApi/Program.cs
@@ -13,19 +13,29 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMapper();//no viene en la plantilla WEBAPI
 builder.Services.AddInjection(builder.Configuration);//no viene en la plantilla WEBAPI
 
+builder.WebHost.ConfigureKestrel(options => //no viene en la plantilla WEBAPI
+{
+    options.ListenAnyIP(4990); // to listen for incoming http connection on port 5001
+                               // options.ListenAnyIP(7001, configure => configure.UseHttps()); // to listen for incoming https connection on port 7001
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
+
+app.UseSwaggerUI();
+
+app.UseRouting();
+
+app.UseSwagger();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapControllers(); //no viene en la plantilla WEBAPI
 
 app.Run();
